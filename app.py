50d5c28edd6d56db933db97cc3e78d465698a64c
zeynnepps <zeynepsalihoglu@gmail.com>
2025-04-24 23:55:45 -0700

    improve

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ st.set_page_config(page_title="RealHaven – AI-Powered Estate Search Platform",
 
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
-    st.image("logo-modified.png", width=150)
-    st.markdown("<h1 style='text-align: center;'>RealHaven</h1>", unsafe_allow_html=True)
-    st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div style="text-align: center;">
+            <img src="https://github.com/zeynnepps/realhaven-capstone/blob/master/logo-modified.png" width="150">
+            <h1 style="margin-top: 10px;">RealHaven</h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
 
 # --- Project Description ---
 st.markdown("""
